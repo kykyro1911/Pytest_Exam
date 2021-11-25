8a68976e9bd433d2cba48e24e6d183a4d3f7c5a2
nono_wang <Nono_Wang@compal.com>
2021-11-25 13:40:52 +0800

    2th

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-"# Pytest_Exam" 
+"# Pytest_Practice"
+---
+
+##Requirements
+python version : python 3.8.0
+opration system : Windows 10
+
+
+##Quick Test:
+
+Step 1:
+use python "pipenv" package to enter the project environments.
+`pipenv shell`
+
+Step 2:
+set the .ini file to declear the **Star Warz** parameters.
+
+Step 3:
+According to the provided test questions and automate the test API and use the python "pytest" module.
+`pytest test_StarWars.py`
+
+and complete to show all of the testing result.
+
+Step 4:
+According to the provided test questions and use "Locust" to make API stress testing.
+`python locustfile.py`
+
+and complete to show all of the testing result.
--- locustfile.py
+++ locustfile.py
@@ -1,20 +1,23 @@
-from locust import HttpUser, TaskSet, task
-import os
+from locust import HttpUser, TaskSet, task, constant
+import os, json
 
 class WebsiteTasks(TaskSet):
-    @task(1)
-    def index(self):
-        self.client.get("/")
-
-    @task(2)
-    def about(self):
-        self.client.get("/about")
+    @task()
+    def re_get(self):
+        resp = self.client.get("people/1/")
+        json_re = json.loads(resp.text)
+        # http status是否為200
+        assert resp.status_code == 200, "HTTP response is " + resp.status_code
+        # vehicles size >= 1
+        assert len(json_re['vehicles']) >= 1, json_re['name'] + "'s vehicles amount is not less than or equal to 1"
+        
 
 class WebsiteUser(HttpUser):
-    task_set = WebsiteTasks
-    min_wait = 5000
-    max_wait = 15000
+    tasks = [WebsiteTasks]
+    host = 'https://swapi.dev/api/'
+    wait_time = constant(1)
+    # stop_timeout = 10
 
 
 if __name__ == "__main__":
-    os.system('locust -f locustfile.py')
+    os.system('locust -f ' + __file__ +  ' --autostart -u 10 -r 10 -t 10s --autoquit 1')
